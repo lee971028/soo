3664d949ee0f9efd33a468fb2031a7e7cfd20b62
soo <lsy971028@gmail.com>
2023-07-04 16:59:21 +0900

    day0704

---
+++ javawork/SistJavaProject/src/day0704/ShopMain_03.java
@@ -0,0 +1,19 @@
+package day0704;
+/*  
+ 1.상품입고   2.상품재고     9.종료
+ 1
+ 상품명? 바나나
+ 수량? 10
+ 가격? 1000
+ 2
+ 번호   상품명    수량    가격    총가격
+ 1     바나나     10     1000  10000 
+ */
+public class ShopMain_03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
