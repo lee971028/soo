beeba6cdfa5269046af1e7a2c67c82735e6833c1
soo <lsy971028@gmail.com>
2023-07-19 11:46:10 +0900

    day0719

---
+++ jswork/htmlwork/js/pop.js
@@ -0,0 +1,9 @@
+function popup1(){
+    
+    window.open('../div_img/beauty1.jpg','pop1','width=400,height=500');
+}
+
+function naver1(){
+
+    window.open('http://m.naver.com','n','width=500,height=500');
+}
